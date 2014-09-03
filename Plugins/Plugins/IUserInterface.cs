bba819ed9c90d76bfa4b66f6c0f2497c747e3480
William Villeneuve <willvill1995@yahoo.ca>
2014-09-03 18:52:38 -0400

    - Added UI plugin project

---
+++ Plugins/Plugins/IUserInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dame.Plugins
+{
+    public interface IUserInterface
+    {
+        // initialize
+        // deinitialize
+        // runMainLoop
+
+        // Loading Indicator (HUD (Loading indicator & text) style)
+        // 
+    }
+}
+
