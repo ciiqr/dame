ce4809be433d0fb6f09e70277b023d4768547abf
William Villeneuve <willvill1995@yahoo.ca>
2014-09-03 20:36:02 -0400

    - Added global constants

--- dame/Constants.cs
+++ dame/Constants.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 
 namespace dame
 {
-    public class Constants
+    public static class Constants
     {
-        public Constants()
-        {
-        }
+        // Application
+        public const string APPLICATION_NAME = "Dame";
+//        public static readonly string APPLICATION_VERSION = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        // Directories
+        public static readonly string APPLICATION_SHARED_DATA_DIRECTORY = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), APPLICATION_NAME); // /usr/share/Dame
+        public static readonly string USER_INTERFACE_PLUGINS_DIRECTORY = Path.Combine(APPLICATION_SHARED_DATA_DIRECTORY, "ui"); // /usr/share/Dame/ui
+
+
+        // Option Keys
+        public const string OPTIONS_UI_KEY = "ui";
+        public const string OPTIONS_DBPATH_KEY = "dbpath";
+
+        // Default Options
+        public const string DEFAULT_OPTION_UI = "gtk";
     }
 }
 
