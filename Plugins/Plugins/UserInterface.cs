f8ed49ed8ebd33faf44d088dc4f93ef231fee94c
William Villeneuve <willvill1995@yahoo.ca>
2014-09-05 09:03:34 -0400

    - Renamed IUserInterface file to UserInterface (since it is no longer an interface)

---
+++ Plugins/Plugins/UserInterface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace dame.Plugins
+{
+    public abstract class UserInterface
+    {
+        abstract public void runMainLoop();
+        abstract public void stopMainLoop();
+
+        // Loading Indicator (HUD (Loading indicator & text) style)
+        // Keybinding creator, preferably using a delegate which is passed with the creation code
+
+        // Windows
+        // Menubars
+        // Toolbars
+        // etc
+
+        // Lots of Events aswell
+    }
+}
+
