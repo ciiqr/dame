a2a736da952e1b3338127a8ecafc8d4b12b361b7
William Villeneuve <willvill1995@yahoo.ca>
2014-09-10 19:38:11 -0400

    - Added try/catch to LoadUserInterface which prints the error about ui plugin likely being outdated, wraps it in a new exception and throws it - Added methods to get the path to a UI plugin - Added method to check if a specific UI plguin exists

--- dame/Plugins.cs
+++ dame/Plugins.cs
@@ -11,10 +11,30 @@ namespace dame.Plugins
     {
         public static UserInterface LoadUserInterface(string uiName)
         {
-            string uiPath = Path.ChangeExtension(Path.Combine(Constants.USER_INTERFACE_PLUGINS_DIRECTORY, uiName), "dll");
-            Assembly uiAssembly = Assembly.LoadFile(uiPath);
-            var uiClasses = uiAssembly.GetTypes().Where(t => t.BaseType == typeof(UserInterface));
-            return (UserInterface)Activator.CreateInstance(uiClasses.First());
+            try
+            {
+                string uiPath = getUIPath(uiName);
+                Assembly uiAssembly = Assembly.LoadFile(uiPath);
+                var uiClasses = uiAssembly.GetTypes().Where(t => t.BaseType == typeof(UserInterface));
+                return (UserInterface)Activator.CreateInstance(uiClasses.First());
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                string errorMessage = String.Format("Could not load user interface \"{0}\", plugin is likely out of date", uiName);
+                Console.WriteLine(errorMessage);
+                Console.WriteLine(ex);
+                throw new Exception(errorMessage, ex);
+            }
+        }
+
+        private static string getUIPath(string uiName)
+        {
+            return Path.ChangeExtension(Path.Combine(Constants.UI_PLUGINS_DIRECTORY, uiName), "dll");
+        }
+
+        public static bool uiExists(string uiName)
+        {
+            return File.Exists(getUIPath(uiName));
         }
     }
 }
