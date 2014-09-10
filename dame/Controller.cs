8f149bcf1f2576113d3a26da2c7f6db5276251a6
William Villeneuve <willvill1995@yahoo.ca>
2014-09-10 19:53:50 -0400

    - Added username parameter to OAuthTokenRecieved - Listen for mainLoopInitializedEvent - SetupUser when main loop is initialized - Started implementing setupUser: - get the username (otherwise prompt for login) - get the auth token (otherwise prompt for re-auth) - check that the database exists & start setting up the ui

--- dame/Controller.cs
+++ dame/Controller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Collections;
 using System.Collections.Generic;
 
 using dame.Data;
@@ -37,32 +38,90 @@ namespace dame
             }
         }
 
+        private Database db = new Database();
+
         public Controller(Dictionary<string, string> options)
         {
             // TODO: Configure based on options
-            // TODO: Initialize ui with either user specified ui OR gtk OR the first one we can find in the ui plugins directory
+            // TODO: Allow specification of a user
+            // TODO: Initialize ui with the first one we can find in the ui plugins directory if we still cant get one
             string userInterfaceName = options.GetElse(Constants.OPTIONS_UI_KEY, Constants.DEFAULT_OPTION_UI);
 
             ui = Plugins.Plugins.LoadUserInterface(userInterfaceName);
+            setupUIEvents();
 
-            // TODO: Initialize user with user specified user OR the user we grab some other way
-            // TODO: Get from initial sync when applicable
-            _username = "willvill1995";
+            // TODO: Testing
+//            Console.WriteLine(Conversion.Convert(DocumentType.HTML, DocumentType.XHTML, "<html><body><p>This is some test test<ul><li>item 1<li>item2<</ul></body>"));
+//            Console.WriteLine(Conversion.Convert(DocumentType.HTML, DocumentType.XHTML, "<html><body><p>This is some test test<p><ul><li>item 1</li><li>item2<</ul></body>"));
+        }
 
+        private void setupUIEvents()
+        {
+            ui.mainLoopInitializedEvent += (sender) =>
+            {
+                setupUser();
+            };
+        }
 
-            Console.WriteLine(Conversion.Convert(DocumentType.HTML, DocumentType.XHTML, "<html><body><p>This is some test test<ul><li>item 1<li>item2<</ul></body>"));
-            Console.WriteLine(Conversion.Convert(DocumentType.HTML, DocumentType.XHTML, "<html><body><p>This is some test test<p><ul><li>item 1</li><li>item2<</ul></body>"));
+        private void setupUser()
+        {
+            #if DEBUG
+            if (DameUser.currentUser == null)
+                DameUser.currentUser = "willvill1995sand";
+            #endif
 
+            _username = DameUser.currentUser;
 
-//            // TODO: Assume the user is the file name of the database in ~/.config/Dame/willvill1995/willvill1995.db
-//            // TODO: Check if we have a user database already
-//            // TODO: Check if we have an auth token for the user
-//            //  - If getAuthToken returns null, display error & prompt for re-authorization
-//            string authToken = Keyring.getAuthToken(_username);
-//
-//            // TODO: Implement OAuth
-//            //            bool isSandbox = true;
-//            //            OAuthTokenRecieved(authToken, isSandbox);
+            if (_username == null)
+            {
+                // No User
+                // TODO: Display login window
+//                ui.windows.login.dislay();
+//                ui.windows.login.dislay.onCompletionHandler += OAuthTokenRecieved;
+            }
+            else
+            {
+                // TODO: How Do I reauth without reprompting
+                // TODO: Check if we have an auth token for the user
+                //  - If getAuthToken returns null, display error & prompt for re-authorization
+                string authToken = Keyring.getAuthToken(_username, out _isSandbox);
+
+                if (authToken == null)
+                {
+                    // TODO: ui.authPrompt.display(string initialUsername = null)
+                    // TODO: Implement OAuth
+
+                }
+                else
+                {
+                    if (!DameUser.databaseExists())
+                    {
+                        // TODO: When database doesn't exist but we had a user in LAST_USER_PATH
+                        Console.WriteLine("Have a username from LAST_USER_PATH, an auth token from the keyring, but no database file...");
+
+                        // TODO: Testing ONLY
+                        bool isSandbox = true;
+                        OAuthTokenRecieved(authToken, _username, isSandbox);
+                    }
+                    else
+                    {
+                        // We have everything
+                        setupUI();
+                    }
+                }
+            }
+        }
+
+        private void setupUI()
+        {
+            // TODO: Start loading data, display main ui, etc.
+
+            // TODO: Testing
+            Async.aSyncLong(delegate
+            {
+                var user = db.getUser(_username);
+
+            });
         }
 
         public void Run()
@@ -70,42 +129,58 @@ namespace dame
             ui.runMainLoop();
         }
 
-        private void OAuthTokenRecieved(string authToken, bool isSandbox)
+        private void OAuthTokenRecieved(string authToken, string username, bool isSandbox)
         {
             _authToken = authToken;
+            _username = username;
             _isSandbox = isSandbox;
 
-            // TODO: Add property for initial sync canceller, or store in syncCanceller
-//            this.initialSyncCanceler = 
-            Async.aSyncLong((initialSyncCanceler) =>
+            DameUser.currentUser = username;
+
+            if (!DameUser.databaseExists())
             {
-                InitialSyncComplete doneHandler = null;
-                doneHandler = (user, premiumInfo) =>
+                // TODO: Add property for initial sync canceller, or store in syncCanceller
+                //            this.initialSyncCanceler = 
+                Async.aSyncLong((initialSyncCanceler) =>
                 {
-                    sync.InitialSyncCompleteHandler -= doneHandler;
-
-                    if (!Keyring.saveAuthToken(_username, _authToken))
+                    InitialSyncComplete doneHandler = null;
+                    doneHandler = (user, premiumInfo) =>
                     {
-                        // TODO: Display an error to the user & allow them to try to login again
-                        Console.WriteLine("");
-                    }
+                        sync.InitialSyncCompleteHandler -= doneHandler;
 
-                    // TODO: Send user & premiumInfo to database
+                        if (!Keyring.saveAuthToken(_username, authToken, isSandbox))
+                        {
+                            // TODO: Display an error to the user & allow them to try to login again
+                            //                        ui.displayPopupMessage(title:"Error Logging In", message:"Could not save authorization token in the keyring");
+                            Console.Write("");
+                        }
 
-                };
-                sync.InitialSyncCompleteHandler += doneHandler;
+                        DameUser.createUserDirectory(DameUser.currentUser);
+                        db.initializeDatabaseWithUser(user);
 
-                var progressHandler = new Progress<SyncProgress>();
-                progressHandler.ProgressChanged += (object sender, SyncProgress e) =>
-                {
-                    Async.main(() =>
+                        // TODO: Send premiumInfo to database
+
+                    };
+                    sync.InitialSyncCompleteHandler += doneHandler;
+
+                    var progressHandler = new Progress<SyncProgress>();
+                    progressHandler.ProgressChanged += (object sender, SyncProgress e) =>
                     {
-                        // TODO: Update UI with progress
-                    });
-                };
+                        Async.main(() =>
+                        {
+                            // TODO: Update UI with progress
+                        });
+                    };
 
-                sync.initialSync(initialSyncCanceler.Token, progressHandler);
-            });
+                    // TODO: Should return user from this method?
+                    // TODO: Currently throws an exception when offline
+                    sync.initialSync(initialSyncCanceler.Token, progressHandler);
+                });
+            }
+            else
+            {
+                // TODO: Multi-User: User is already attached, resave auth token & switch to user
+            }
         }
     }
 }
