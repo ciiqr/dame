f27f8c02dc25c2175d8e2761704ab131fc584912
William Villeneuve <willvill1995@yahoo.ca>
2014-09-10 19:55:32 -0400

    - Added constants for": - user directories/files - application version - application resources - database file extension

--- dame/Constants.cs
+++ dame/Constants.cs
@@ -8,19 +8,34 @@ namespace dame
     {
         // Application
         public const string APPLICATION_NAME = "Dame";
-//        public static readonly string APPLICATION_VERSION = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        public static readonly Version APPLICATION_VERSION = Assembly.GetExecutingAssembly().GetName().Version;
 
         // Directories
         public static readonly string APPLICATION_SHARED_DATA_DIRECTORY = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), APPLICATION_NAME); // /usr/share/Dame
-        public static readonly string USER_INTERFACE_PLUGINS_DIRECTORY = Path.Combine(APPLICATION_SHARED_DATA_DIRECTORY, "ui"); // /usr/share/Dame/ui
+        public static readonly string UI_PLUGINS_DIRECTORY = Path.Combine(APPLICATION_SHARED_DATA_DIRECTORY, "ui"); // /usr/share/Dame/ui
 
+        // OS User
+        public static readonly string USER_DIRECTORY = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        public static readonly string USER_CONFIG_DIRECTORY = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), APPLICATION_NAME);
+        public static readonly string USER_APPLICATION_CACHE_DIRECTORY = Path.Combine(USER_DIRECTORY, ".cache", APPLICATION_NAME); // TODO: Implement XDG.BaseDirectory
+
+        // Files
+        public static readonly string LAST_USER_PATH = Path.Combine(USER_CONFIG_DIRECTORY, "lastUser"); // ~/.config/Dame/lastUser
+
+        // Resources
+        // TODO: Move to APPLICATION_SHARED_DATA_DIRECTORY/Resources
+        public static readonly string RESOURCES_DIRECTORY = "Resources"; // ./Resources/InitialDatabase.sql
+        public static readonly string INITIAL_DATABASE_PATH = Path.Combine(RESOURCES_DIRECTORY, "InitialDatabase.sql"); // ./Resources/InitialDatabase.sql
 
         // Option Keys
         public const string OPTIONS_UI_KEY = "ui";
-        public const string OPTIONS_DBPATH_KEY = "dbpath";
+        public const string OPTIONS_USERNAME_KEY = "username";
 
         // Default Options
         public const string DEFAULT_OPTION_UI = "gtk";
+
+        // Miscellaneous
+        public const string DATABASE_FILE_EXTENSION = "db";
     }
 }
 
